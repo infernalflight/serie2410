7030d3a50efcb1f72c45bd5da5099585dc344c1e
fred <ffotre@campus-eni.fr>
2025-07-02 15:41:39 +0200

    Creation Controllers

---
+++ src/Controller/SerieController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Serie;
+use App\Repository\SerieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class SerieController extends AbstractController
+{
+
+    #[Route('/serie/test', name:'serie_test')]
+    public function test(EntityManagerInterface $em): Response
+    {
+        $serie = new Serie();
+        $serie->setName("Walking Dead")
+            ->setOverview("Les zombies ont dominé la terre")
+            ->setStatus("Ended")
+            ->setGenres("Horror, Thriller, Gore")
+            ->setFirstAirDate(new \DateTime("2010-03-25"))
+            ->setLastAirDate(new \DateTime("2022-10-03"))
+            ->setDateCreated(new \DateTime());
+
+        $em->persist($serie);
+        $em->flush();
+
+        return new Response("Objet crée avec succès");
+    }
+
+    #[Route('/serie/list', name:'serie_list')]
+    public function list(SerieRepository $serieRepository): Response
+    {
+        //$series = $serieRepository->findAll();
+
+        $series = $serieRepository->findBy(
+            ['status' => "returning", 'genres' => "Comedy / Drama"],
+            ['name' => 'DESC'],
+        );
+
+        $series = $serieRepository->getSeriesByPopularity(50);
+
+        $series = $serieRepository->getSeriesWithDql(75);
+
+        return $this->render('serie/list.html.twig', [
+            'series' => $series,
+        ]);
+    }
+}
